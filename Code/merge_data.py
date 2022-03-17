3828274f708d2e00a83da4fdd3bed7bc2634a42e
kch0p <89766547+kch0p@users.noreply.github.com>
2022-03-17 15:38:58 -0700

    Project Setup

---
+++ Code/merge_data.py
@@ -0,0 +1,7 @@
+import glob
+import pandas as pd
+
+non_ripa_df = pd.read_csv('C:\Users\kharr\Documents\GitHub\Berkeley-PD-ISF-110\Data\Berkeley_PD_-_Stop_Data__Jan_26__2015_to_Sep_30__2020_.csv')
+
+ripa_df = pd.read_csv('C:\Users\kharr\Documents\GitHub\Berkeley-PD-ISF-110\Data\Berkeley_PD_-_Stop_Data__October_1__2020_-_Present.csv')
+
