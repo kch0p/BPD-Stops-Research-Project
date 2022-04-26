5ea793c48f9f85894fbf09d56ba2aaee6c5bb8d2
kch0p <89766547+kch0p@users.noreply.github.com>
2022-04-25 17:25:02 -0700

    Coding Variables for Regression Model

--- Code/Logistic Regression Python Model.py
+++ Code/Logistic Regression Python Model.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 from matplotlib import pyplot as plt
+import numpy as np
+import warnings
+
+
+warnings.filterwarnings("ignore")
 
 df = pd.read_csv('formatted_allstops(expanded)(2).csv')
 
@@ -21,24 +26,72 @@ df.perceivedraceorethnicity[df.perceivedraceorethnicity == 'White'] = 2
 df.perceivedraceorethnicity[df.perceivedraceorethnicity == 'Black/African American'] = 3
 df.perceivedraceorethnicity[df.perceivedraceorethnicity ==  'Black'] = 3
 
-# df.perceivedraceorethnicity[df.perceivedraceorethnicity=="Hispanic/Latino"] = 4 
+df.perceivedraceorethnicity[df.perceivedraceorethnicity=="Hispanic/Latino"] = 4 
 df.perceivedraceorethnicity[df.perceivedraceorethnicity=="Hispanic"] = 4
 
 df.perceivedraceorethnicity[df.perceivedraceorethnicity == 'Asian'] = 5
-df.perceivedraceorethnicity[df.perceivedraceorethnicity != 2 &
-                            df.perceivedraceorethnicity != 3 &
-                            df.perceivedraceorethnicity != 4 & 
-                            df.perceivedraceorethnicity != 5] = 1
+
+for i in range(df.index.size):
+    if df.loc[i,'perceivedraceorethnicity'] in (2,3,4,5):
+        continue
+    else:
+        df.loc[i,'perceivedraceorethnicity'] = 1
+
+# print(f'the value counts for race are: \n {df.perceivedraceorethnicity.value_counts()}')
 
 
 
 
 
-# gen race = 1
-# replace race = 2 if perceivedraceorethnicity=="White"  
-# replace race = 3 if perceivedraceorethnicity=="Black/African American" |  perceivedraceorethnicity=="Black"
-# replace race = 4 if perceivedraceorethnicity=="Hispanic/Latino" |  perceivedraceorethnicity=="Hispanic"
-# replace race = 5 if perceivedraceorethnicity=="Asian"   
-# replace race = 1 if !inlist(race,1,2,3,4,5)
-# lab def race 1 "Other" 2 "White" 3 "Black/African American" 4 "Hispanic/Latino" 5 "Asian" 
-# lab value race race
+df.gender[df.gender == 'Male'] = 1
+df.gender[df.gender == 'Female'] = 2
+for i in range(df.index.size):
+    if df.loc[i,'gender'] in (1,2):
+        continue
+    else:
+        df.loc[i,'gender'] = 3
+
+
+
+
+df['arrest'] = df['resultofstop']
+
+for i in range(df.index.size):
+    substring = 'arrest'
+    fullstring = df.loc[i, 'arrest'] 
+    
+    if substring in fullstring:
+        df.loc[i, 'arrest'] = 1
+    else:
+        df.loc[i, 'arrest'] = 0
+# print(f'the value counts for arrests are: \n {df.arrest.value_counts()}')
+
+
+df.typeofstop[df.typeofstop == 'Vehicle'] = 1
+df.typeofstop[df.typeofstop == 'Pedestrian'] = 2 
+df.typeofstop[df.typeofstop == 'Bicycle'] = 3
+
+
+
+
+df['black'] = 0
+df['white'] = 0
+df['asian'] = 0
+df['hispanic'] = 0
+for i in range(df.index.size):
+    if df.loc[i,'perceivedraceorethnicity'] == 3:
+        df.loc[i,'black'] = 1
+    elif df.loc[i,'perceivedraceorethnicity'] == 2:
+        df.loc[i,'white'] = 1
+    elif df.loc[i,'perceivedraceorethnicity'] == 5:
+        df.loc[i,'asian'] = 1
+    elif df.loc[i,'perceivedraceorethnicity'] == 4:
+        df.loc[i,'hispanic'] = 1
+
+
+
+
+
+          
+          
+          
